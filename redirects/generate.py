a537477a6e33cbfcba1fbb5fe55f5c5e00ec0b74
Ondrej Sika <ondrej@ondrejsika.com>
2019-12-19 10:45:40 +0100

    redirects - ondrejsika.ch -> sika.io

--- redirects/generate.py
+++ redirects/generate.py
@@ -6,6 +6,7 @@ _CONFIG = {
     'https://training.kubernetes.lu/': ['kubernetes.lu'],
     'https://trainera.io/': ['trainera.cz'],
     'https://ondrej-sika.cz/': ['ondrejsika.cz'],
+    'https://sika.io/': ['ondrejsika.ch'],
 }
 
 CONFIG = {}
