a2a535dcf42075e5dbd236a02a9d96283802a3e7
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-03 11:24:55 +0100

    default-domain-pages - remove Ondrej Sika's domain

--- default-domain-page/pages/index.js
+++ default-domain-page/pages/index.js
@@ -1,14 +1,10 @@
 import Head from "next/head";
 
 const page = props => {
-  let domain = props.domain;
-
-  let title = `${domain} - Ondrej Sika's Domain`;
-
-  return (
+  return(
     <div>
-      <Head>{title}</Head>
-      <h1>{title}</h1>
+      <Head>{props.domain}</Head>
+      <h1>{props.domain}</h1>
       <p>You can reach me on <a href="mailto:ondrej@ondrejsika.com">ondrej@ondrejsika.com</a>.</p>
     </div>
   );
