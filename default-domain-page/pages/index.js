c84a8a154cffa3f2405fba5212adb716635c7b31
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-25 13:18:02 +0100

    default-domain-page - center content

--- default-domain-page/pages/index.js
+++ default-domain-page/pages/index.js
@@ -10,11 +10,14 @@ const page = props => {
           rel="stylesheet"
         />
       </Head>
-      <h1>{props.domain}</h1>
-      <p>
-        You can reach me on{" "}
-        <a href="mailto:ondrej@ondrejsika.com">ondrej@ondrejsika.com</a>.
-      </p>
+      <center>
+        <h1>{props.domain}</h1>
+        <p>
+          You can reach me on{" "}
+          <a href="mailto:ondrej@ondrejsika.com">ondrej@ondrejsika.com</a>.
+        </p>
+      </center>
+
       <style jsx global>{`
         h1 {
           font-family: "IBM Plex Mono", monospace;
