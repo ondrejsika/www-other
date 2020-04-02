49f511a1cf8811e951c34f2825a3ed577eaafe00
Ondrej Sika <ondrej@ondrejsika.com>
2020-04-02 16:13:47 +0200

    redirects - remove sikalabs.com

--- redirects/generate.py
+++ redirects/generate.py
@@ -6,7 +6,7 @@ _CONFIG = {
     'https://training.kubernetes.lu/': ['kubernetes.lu'],
     'https://trainera.io/': ['trainera.cz'],
     'https://ondrej-sika.cz/': ['ondrejsika.cz'],
-    'https://sika.io/': ['ondrejsika.ch', 'docker-training.eu', 'git-training.eu', 'sikalabs.com'],
+    'https://sika.io/': ['ondrejsika.ch', 'docker-training.eu', 'git-training.eu'],
     'https://ondrejsika.com/': ['os.oxs.cz'],
     'https://stepanka.jebozi.cz/': ['jebozi.cz', 'www.stepanka.jebozi.cz'],
 }
